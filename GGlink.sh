422b8f53ffde3b084430f48cbbed37a1251b7d3b
Mario Ragazzi <m@ragazziconsulting.com>
2018-12-11 16:01:46 +0100

    Update for Ghost 1.x

--- GGlink.sh
+++ GGlink.sh
@@ -2,25 +2,24 @@
 
 #################################################################################
 # I assume here that:
-#     you have a ghost instance installed in /home/ghost/yourblogtitle
-#     you will keep the script and the git local repository in /home/ghost/$backupdir
-#     you have created a git repository in /home/ghost/$backupdir/$gitrepo
+#     you have a ghost instance installed in /$rootdir
+#     you will keep the script and the git local repository in /home/$user/backup
+#     you have created a git repository in /home/$user/backup/$gitrepo
 #         ideally cloning a remote repository on a service like GitHub or GitLab
 # This script is run hourly as a cronjob for user ghost
 ##################################################################################
+
 ### Read configuration parameters ###
 cd $HOME
-source GGprivate/GGconfig.txt
-
-ghostdbpath=$rootdir/$blogslug"/content/data"
+source $HOME/GGprivate/GGconfig.txt
 
 #echo $ghostdbpath/$ghostoriginaldb
 #echo $rootdir/GGprivate/ghost-backup.db
 
 if [[ $(find $ghostdbpath -mmin -60 -type f -name $ghostoriginaldb  2>/dev/null) ]]
    then
-      rsync -av $ghostdbpath/$ghostoriginaldb $rootdir/GGprivate/ghost-backup.db
-      python $rootdir/Ghost2Git/GGlink.py
+      rsync -av $ghostdbpath/$ghostoriginaldb $HOME/GGprivate/ghost-backup.db
+      python $HOME/Ghost2Git/GGlink1.py
       cd $rootdir/$gitreponame
       git push
 fi
